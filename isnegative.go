1f7a80132be63b71a14441f22dce302b06d93f9d
LCHARLOPEAU <luc.charlopeau@ynov.com>
2021-10-14 10:17:15 +0200

    isnegative

---
+++ isnegative.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func IsNegative(nb int) {
+	if nb >= 0 {
+		z01.PrintRune(rune(84))
+	} else {
+		z01.PrintRune(rune(70))
+
+	}
+	z01.PrintRune(rune('\n'))
+}
+
+// func main() {
+
+//IsNegative(1)
+//IsNegative(-1)
+//}
