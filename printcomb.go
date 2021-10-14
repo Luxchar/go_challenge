9911797f2c2d52ffa5a6fcd929f395409ef3e1d8
LCHARLOPEAU <luc.charlopeau@ynov.com>
2021-10-14 15:49:07 +0200

    printcombo

---
+++ printcomb.go
@@ -0,0 +1,32 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func PrintComb() {
+	var a, b, c int
+	for i := 0; i < 1000; i++ {
+		c++
+		if c == 10 {
+			b++
+			c = 0
+		}
+		if b == 10 {
+			a++
+			b = 0
+		}
+		if c > b && c > a && b > a {
+			z01.PrintRune(rune(48 + a))
+			z01.PrintRune(rune(48 + b))
+			z01.PrintRune(rune(48 + c))
+			z01.PrintRune(rune(44))
+			z01.PrintRune(32)
+		}
+
+	}
+}
+
+/*
+func main() {
+	PrintComb()
+}
+*/
