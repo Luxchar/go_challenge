8f63d71abf03626a35e5394137d8c7c17f7d957e
LCHARLOPEAU <luc.charlopeau@ynov.com>
2021-10-14 09:23:36 +0200

    dossieralphabet

---
+++ printalphabet/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func main() {
+	for compteur := 97; compteur < 123; compteur++ {
+		z01.PrintRune(rune(compteur))
+	}
+	z01.PrintRune(rune('\n'))
+}
