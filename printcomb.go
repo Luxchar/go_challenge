72bea49d4b5b84b8e05104bf33dad02b86007340
LCHARLOPEAU <luc.charlopeau@ynov.com>
2021-10-14 16:04:16 +0200

    cbon

--- printcomb.go
+++ printcomb.go
@@ -4,8 +4,15 @@ import "github.com/01-edu/z01"
 
 func PrintComb() {
 	var a, b, c int
-	for i := 0; i < 1000; i++ {
+	for i := 0; i < 789; i++ {
 		c++
+		if i == 788 {
+			z01.PrintRune(rune(48 + a))
+			z01.PrintRune(rune(48 + b))
+			z01.PrintRune(rune(48 + c))
+			z01.PrintRune(rune('\n'))
+			break
+		}
 		if c == 10 {
 			b++
 			c = 0
@@ -23,7 +30,6 @@ func PrintComb() {
 		}
 
 	}
-	z01.PrintRune(rune('\n'))
 }
 
 /*
