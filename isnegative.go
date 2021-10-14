eb99859c3ee0784a46298bc8a935ec453feb94b8
LCHARLOPEAU <lcharlopeau@noreply.git.ytrack.learn.ynov.com>
2021-10-14 08:33:25 +0000

    Update 'isnegative.go'

--- isnegative.go
+++ isnegative.go
@@ -6,9 +6,9 @@ import (
 
 func IsNegative(nb int) {
 	if nb >= 0 {
-		z01.PrintRune(rune(84))
-	} else {
 		z01.PrintRune(rune(70))
+	} else {
+		z01.PrintRune(rune(84))
 
 	}
 	z01.PrintRune(rune('\n'))
