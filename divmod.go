db67bc95ab0f145a566340987d280386daa11e42
LCHARLOPEAU <luc.charlopeau@ynov.com>
2021-10-15 14:59:18 +0200

    divmod

---
+++ divmod.go
@@ -0,0 +1,19 @@
+package piscine
+
+func DivMod(a int, b int, div *int, mod *int) {
+	*div = a / b
+	*mod = a % b
+
+}
+
+/*
+func main() {
+	a := 13
+	b := 2
+	var div int
+	var mod int
+	DivMod(a, b, &div, &mod)
+	fmt.Println(div)
+	fmt.Println(mod)
+}
+*/
