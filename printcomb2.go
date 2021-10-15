45a404f714ca245e403e129d3fcff05d22898302
LCHARLOPEAU <luc.charlopeau@ynov.com>
2021-10-15 15:32:42 +0200

    printcombéé2222

--- printcomb2.go
+++ printcomb2.go
@@ -5,10 +5,6 @@ import "github.com/01-edu/z01"
 func PrintComb2() {
 	var a, b, c, d int
 	for i := 0; i < 10000; i++ {
-		if a == 9 && b == 8 && c == 9 && d == 9 {
-			z01.PrintRune(rune('\n'))
-			break
-		}
 		d++
 		if d == 10 {
 			c++
@@ -24,6 +20,15 @@ func PrintComb2() {
 		}
 		if a == 10 {
 			a = 0
+		}
+		if a == 9 && b == 8 && c == 9 && d == 9 {
+			z01.PrintRune(rune(48 + a))
+			z01.PrintRune(rune(48 + b))
+			z01.PrintRune(rune(32))
+			z01.PrintRune(rune(48 + c))
+			z01.PrintRune(rune(48 + d))
+			z01.PrintRune(rune('\n'))
+			break
 		}
 		if a*10+b < c*10+d {
 			z01.PrintRune(rune(48 + a))
