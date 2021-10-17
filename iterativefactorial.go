88c25e185d700171cfa3b19b3e70935a29181092
LCHARLOPEAU <luc.charlopeau@ynov.com>
2021-10-17 23:03:00 +0200

    factorial

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -2,11 +2,8 @@ package piscine
 
 func IterativeFactorial(nb int) int {
 	var factorielle int = 1
-	if nb < 0 {
-		return 0
-	}
 	if nb == 1 || nb == 0 {
-		return nb
+		return 0
 	} else {
 		for i := 1; i <= nb; i++ {
 			factorielle *= i
