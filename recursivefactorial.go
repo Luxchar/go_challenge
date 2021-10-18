eac2812c6538b39316eb525eeac369a45c50cae4
LCHARLOPEAU <luc.charlopeau@ynov.com>
2021-10-18 10:34:03 +0200

    factorial

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,15 +1,18 @@
 package piscine
 
 func RecursiveFactorial(nb int) int {
-	if nb == 1 || nb == 0 || nb < 0 {
-		return nb
+	if nb == 0 {
+		return 1
+	}
+	if nb < 0 || nb > 99 {
+		return 0
 	}
 	return nb * RecursiveFactorial(nb-1)
 }
 
 /*
 func main() {
-	arg := 4
-	fmt.Println(IterativeFactorial(arg))
+	arg := 5
+	fmt.Println(RecursiveFactorial(arg))
 }
 */
