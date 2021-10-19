639c4d9ced30e2b75d7d565b6e45221fa7a48e22
LCHARLOPEAU <luc.charlopeau@ynov.com>
2021-10-19 09:50:27 +0200

    strreversed

---
+++ strrev.go
@@ -0,0 +1,19 @@
+package piscine
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func StrRev(s string) string {
+	for i := piscine.strlen(s); i < 0; i-- {
+		z01.PrintRune(r)
+	}
+}
+
+/*
+func main() {
+	s := "Hello World!"
+	s = StrRev(s)
+	fmt.Println(s)
+}
+*/
