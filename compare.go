9518683a69e1b8995ba557f8dd68c1b1e74b7941
LCHARLOPEAU <luc.charlopeau@ynov.com>
2021-10-19 14:45:04 +0200

    compare

---
+++ compare.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Compare(a, b string) int {
+	if a == b {
+		return 0
+	}
+	if a < b {
+		return -1
+	}
+	return 1
+}
+
+func main() {
+	fmt.Println(Compare("Hello!", "Hello!"))
+	fmt.Println(Compare("Salut!", "lut!"))
+	fmt.Println(Compare("Ola!", "Ol"))
+}
