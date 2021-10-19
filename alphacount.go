0a1ecdc9076edaabc804a86558a4a299be1d8c42
LCHARLOPEAU <luc.charlopeau@ynov.com>
2021-10-19 15:02:25 +0200

    alphacount

---
+++ alphacount.go
@@ -0,0 +1,26 @@
+package piscine
+
+func AlphaCount(s string) int {
+	count := 0
+	for _, r := range s {
+		for compteur := 97; compteur < 123; compteur++ {
+			if string(r) == string(compteur) {
+				count++
+			}
+		}
+		for compteur := 65; compteur < 90; compteur++ {
+			if string(r) == string(compteur) {
+				count++
+			}
+		}
+	}
+	return count
+}
+
+/*
+func main() {
+	s := "Hello 78 World!    4455 /"
+	nb := AlphaCount(s)
+	fmt.Println(nb)
+}
+*/
