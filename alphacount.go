915d118b83365f58151bf14675bc3ac1897ccd2d
LCHARLOPEAU <luc.charlopeau@ynov.com>
2021-10-19 15:05:04 +0200

    alphacount

--- alphacount.go
+++ alphacount.go
@@ -3,12 +3,12 @@ package piscine
 func AlphaCount(s string) int {
 	count := 0
 	for _, r := range s {
-		for compteur := 97; compteur < 122; compteur++ {
+		for compteur := 97; compteur <= 122; compteur++ {
 			if string(r) == string(compteur) {
 				count++
 			}
 		}
-		for compteur := 65; compteur < 90; compteur++ {
+		for compteur := 65; compteur <= 90; compteur++ {
 			if string(r) == string(compteur) {
 				count++
 			}
