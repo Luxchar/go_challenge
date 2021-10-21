d925dd07d66897130eedabd085a92206a8ce7ecb
LCHARLOPEAU <luc.charlopeau@ynov.com>
2021-10-21 11:43:02 +0200

    printprogramname

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -2,15 +2,13 @@ package main
 
 import (
 	"os"
-	"path/filepath"
 
 	"github.com/01-edu/z01"
 )
 
 func main() {
-	path := os.Args[0]
-	path = filepath.Base(path)
-	for _, r := range path {
+	s := os.Args[0]
+	for _, r := range s {
 		z01.PrintRune(r)
 	}
 	z01.PrintRune(rune('\n'))
