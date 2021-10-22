a32e7a5df26c537634ea43502a00f3c70865e133
LCHARLOPEAU <luc.charlopeau@ynov.com>
2021-10-22 10:20:31 +0200

    sortparams

--- sortparams/main.go
+++ sortparams/main.go
@@ -11,14 +11,15 @@ func main() {
 	for i := len(arg) - 1; i >= 1; i-- {
 		for j := len(arg) - 1; j >= 1; j-- {
 			if arg[int(i)] > arg[int(j)] {
-				print(arg[i])
 				arg[i], arg[j] = arg[j], arg[i]
 			}
 		}
 	}
-	for _, k := range arg {
-		print(k)
-		z01.PrintRune('\n')
+	for i := 1; i < len(arg); i++ {
+		for _, k := range arg[i] {
+			z01.PrintRune(k)
+			z01.PrintRune('\n')
+		}
 	}
 }
 
