ceaac5e14c1ab66c2e4d05c2044da205d5562b81
LCHARLOPEAU <luc.charlopeau@ynov.com>
2021-10-25 10:12:24 -0700

    concatparams

--- concatparams.go
+++ concatparams.go
@@ -2,9 +2,13 @@ package piscine
 
 func ConcatParams(args []string) string {
 	a := ""
+	index := len(args) - 1
 	for _, i := range args {
 		a += i
-		a += "\n"
+		if index < 0 {
+			a += "\n"
+			index--
+		}
 
 	}
 	return a
