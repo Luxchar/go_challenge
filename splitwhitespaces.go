1312a5b91a2fb9503a3d0db55ba847381021b26e
LCHARLOPEAU <luc.charlopeau@ynov.com>
2021-10-25 10:56:23 -0700

    splitwhite

---
+++ splitwhitespaces.go
@@ -0,0 +1,21 @@
+package piscine
+
+func SplitWhiteSpaces(s string) []string {
+	a := []string{}
+	mot := ""
+	index := 1
+	for _, i := range s {
+		if len(s) == index {
+			mot += string(i)
+			a = append(a, mot)
+		}
+		if string(i) == " " {
+			a = append(a, mot)
+			mot = ""
+		} else {
+			mot += string(i)
+		}
+		index++
+	}
+	return a
+}
