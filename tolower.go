8675099015916cb39434e11f4b33c6822c5f193f
LCHARLOPEAU <luc.charlopeau@ynov.com>
2021-10-26 15:19:59 -0700

    verslebas

--- tolower.go
+++ tolower.go
@@ -3,7 +3,7 @@ package piscine
 func ToLower(s string) string {
 	a := ""
 	for _, i := range s {
-		if int(i) > 65 && int(i) <= 90 {
+		if int(i) >= 65 && int(i) <= 90 {
 			a += string(int(i) + 32)
 		} else {
 			a += string(int(i))
