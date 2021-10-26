93babc472e4240c0f6715e5c0e3ec0fbc7e9c177
LCHARLOPEAU <luc.charlopeau@ynov.com>
2021-10-26 14:56:35 -0700

    verslehaut

--- toupper.go
+++ toupper.go
@@ -3,20 +3,11 @@ package piscine
 func ToUpper(s string) string {
 	a := ""
 	for _, i := range s {
-		if IsLower(string(i)) {
+		if int(i) <= 96 || int(i) >= 123 {
 			a += string(int(i) - 32)
 		} else {
 			a += string(int(i))
 		}
 	}
 	return a
-}
-
-func IsLower(s string) bool {
-	for _, i := range s {
-		if int(i) <= 96 || int(i) >= 123 {
-			return false
-		}
-	}
-	return true
 }
