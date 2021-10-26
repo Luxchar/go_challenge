ffa7c90b9920ba8c2d6ec56550bf587d15e27fdd
LCHARLOPEAU <luc.charlopeau@ynov.com>
2021-10-26 15:19:37 -0700

    verslebas

--- tolower.go
+++ tolower.go
@@ -1,13 +1,13 @@
 package piscine
 
 func ToLower(s string) string {
-	z := ""
+	a := ""
 	for _, i := range s {
-		if int(i) > 65 && int(i) < 90 {
-			z += string(int(i) + 32)
+		if int(i) > 65 && int(i) <= 90 {
+			a += string(int(i) + 32)
 		} else {
-			z += string(int(i))
+			a += string(int(i))
 		}
 	}
-	return z
+	return a
 }
--- toupper.go
+++ toupper.go
@@ -3,7 +3,7 @@ package piscine
 func ToUpper(s string) string {
 	a := ""
 	for _, i := range s {
-		if int(i) > 96 && int(i) < 123 {
+		if int(i) > 96 && int(i) <= 123 {
 			a += string(int(i) - 32)
 		} else {
 			a += string(int(i))
