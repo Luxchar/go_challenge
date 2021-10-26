f5122356b8d2ac7a36b8b4d40d3c57e8ef358ee4
LCHARLOPEAU <luc.charlopeau@ynov.com>
2021-10-26 14:15:51 -0700

    estalpha

--- isalpha.go
+++ isalpha.go
@@ -2,7 +2,7 @@ package piscine
 
 func IsAlpha(s string) bool {
 	for _, i := range s {
-		if (int(i) <= 96 || int(i) >= 123) && (int(i) < 65 || int(i) > 90) && (int(i) <= 48 || int(i) >= 57) {
+		if (int(i) <= 96 || int(i) >= 123) && (int(i) < 65 || int(i) > 90) && (int(i) < 48 || int(i) > 58) {
 			return false
 		}
 	}
