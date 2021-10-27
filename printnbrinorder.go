a972855d16e402bf94ef78a09d9f2eb9098efbaa
LCHARLOPEAU <luc.charlopeau@ynov.com>
2021-10-26 23:33:36 -0700

    printenordrecroissantamanger

---
+++ printnbrinorder.go
@@ -0,0 +1,26 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintNbrInOrder(n int) {
+	order := []int{}
+	if n == 0 {
+		z01.PrintRune(rune('0'))
+	} else {
+		for i := 0; i < n; i++ {
+			valeur := n % 10
+			n /= 10
+			order = append(order, valeur)
+		}
+		for i := 0; i < len(order); i++ {
+			for j := 0; j < len(order); j++ {
+				if order[i] < order[j] {
+					order[i], order[j] = order[j], order[i]
+				}
+			}
+		}
+		for i := 0; i < len(order); i++ {
+			z01.PrintRune(rune(48 + order[i]))
+		}
+	}
+}
