2d91f69ed6d023cf9b933a1c3e2cdf9b8c2ef733
LCHARLOPEAU <luc.charlopeau@ynov.com>
2021-10-28 10:56:52 -0700

    affichefichier

--- displayfile/main.go
+++ displayfile/main.go
@@ -6,25 +6,21 @@ import (
 	"os"
 )
 
-func displayfilename(args string) string {
-	fichier := os.Args[1]
-	file, err := os.Open(fichier)
-	defer func() {
-		if err = file.Close(); err != nil {
-			print("")
-		}
-	}()
-
-	b, err := ioutil.ReadAll(file)
-	myString := string(b)
-	return myString
-}
-
 func main() {
 	if len(os.Args[1:]) > 1 {
 		fmt.Print("Too many arguments")
 	} else if len(os.Args[1:]) >= 1 {
-		fmt.Print(displayfilename(os.Args[1]))
+		fichier := os.Args[1]
+		file, err := os.Open(fichier)
+		defer func() {
+			if err = file.Close(); err != nil {
+				print("")
+			}
+		}()
+
+		b, err := ioutil.ReadAll(file)
+		myString := string(b)
+		fmt.Print(myString)
 	} else {
 		fmt.Print("File name missing")
 	}
