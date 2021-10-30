2b5e4713e48e1d0916307e3baa8ec719b6f69919
LCHARLOPEAU <luc.charlopeau@ynov.com>
2021-10-29 17:11:12 -0700

    carte

---
+++ map.go
@@ -0,0 +1,12 @@
+package piscine
+
+func Map(f func(int) bool, a []int) []bool {
+	arr := []bool{}
+	for _, i := range a {
+		if f(i) {
+			arr = append(arr, true)
+		}
+		arr = append(arr, false)
+	}
+	return arr
+}
