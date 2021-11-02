1c234e046a76d4039deb4aa95beaf549ebebf758
LCHARLOPEAU <luc.charlopeau@ynov.com>
2021-11-02 14:06:39 +0100

    listepoussebas

--- listpushback.go
+++ listpushback.go
@@ -1,5 +1,15 @@
 package piscine
 
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
 func ListPushBack(l *List, data interface{}) {
 	node := &NodeL{Data: data, Next: nil}
 	if l.Tail == nil {
