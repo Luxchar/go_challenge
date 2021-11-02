94d288fbc0e5c6b71fa2138b38c7f1f20f07e239
LCHARLOPEAU <luc.charlopeau@ynov.com>
2021-11-02 14:34:10 +0100

    listepoussedevant

--- listsize.go
+++ listsize.go
@@ -1,16 +1,5 @@
 package piscine
 
-type NodeL struct {
-	Data interface{}
-	Next *NodeL
-}
-
-type List struct {
-	length int
-	Head   *NodeL
-	Tail   *NodeL
-}
-
 func ListPushFront(l *List, data interface{}) {
 	node := &NodeL{Data: data, Next: l.Head}
 	l.Head = node
