921436cce13d98980a3c46c14716eef830644daf
LCHARLOPEAU <luc.charlopeau@ynov.com>
2021-11-02 11:52:53 +0100

    esttrie

--- issorted.go
+++ issorted.go
@@ -1,20 +1,22 @@
 package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
-	croissant := true
-	if a[0]-a[1] > 0 {
-		croissant = false
-	}
-	for i, v := range a {
-		for j := i; j < len(a); j++ {
-			if croissant {
-				if f(v, a[j]) > 0 {
-					return false
+	if len(a) > 1 {
+		croissant := true
+		if a[0]-a[1] > 0 {
+			croissant = false
+		}
+		for i, v := range a {
+			for j := i; j < len(a); j++ {
+				if croissant {
+					if f(v, a[j]) > 0 {
+						return false
+					}
 				}
-			}
-			if croissant != true {
-				if f(v, a[j]) < 0 {
-					return false
+				if croissant != true {
+					if f(v, a[j]) < 0 {
+						return false
+					}
 				}
 			}
 		}
