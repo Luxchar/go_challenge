4752ef7323cd88772d98e796cf77eafc1dec2221
LCHARLOPEAU <luc.charlopeau@ynov.com>
2021-11-02 12:05:20 +0100

    affichefichier

--- displayfile/main.go
+++ displayfile/main.go
@@ -10,13 +10,8 @@ func main() {
 	if len(os.Args[1:]) > 1 {
 		fmt.Println("Too many arguments")
 	} else if len(os.Args[1:]) >= 1 {
-		file, err := os.Open(os.Args[1])
-		defer func() {
-			if err = file.Close(); err != nil {
-				return
-			}
-		}()
-		b, err := ioutil.ReadAll(file)
+		file, _ := os.Open(os.Args[1])
+		b, _ := ioutil.ReadAll(file)
 		fmt.Println(string(b))
 	} else {
 		fmt.Println("File name missing")
