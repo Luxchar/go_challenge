01c9bceefd866a20227861d4cfa2c1c2499a07c5
LCHARLOPEAU <luc.charlopeau@ynov.com>
2021-11-02 09:53:48 +0100

    esttrie

---
+++ issorted.go
@@ -0,0 +1,14 @@
+package piscine
+
+func IsSorted(f func(a, b int) int, a []int) bool {
+	index := 0
+	for _, i := range a {
+		for j := index; j < len(a); j++ {
+			if f(i, a[j]) < 0 {
+				return false
+			}
+		}
+		index++
+	}
+	return true
+}
