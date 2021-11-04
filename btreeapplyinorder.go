2b9028b5210f8bb465ae304b65313da1a13867f4
LCHARLOPEAU <luc.charlopeau@ynov.com>
2021-11-04 14:57:21 +0100

    arbre binaire

--- btreeapplyinorder.go
+++ btreeapplyinorder.go
@@ -3,7 +3,7 @@ package piscine
 func BTreeApplyInorder(root *TreeNode, f func(...interface{}) (int, error)) {
 	if root != nil {
 		BTreeApplyInorder(root.Left, f)
-		f(root)
+		f(root.Data)
 		BTreeApplyInorder(root.Right, f)
 	}
 }
