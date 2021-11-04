5cf6c760b305f05d63d04d9e1801232394bcdeeb
LCHARLOPEAU <luc.charlopeau@ynov.com>
2021-11-04 08:46:57 +0100

    liste a

--- listat.go
+++ listat.go
@@ -1,29 +1,4 @@
-package main
-
-import (
-	"fmt"
-)
-
-type NodeL struct {
-	Data interface{}
-	Next *NodeL
-}
-
-type List struct {
-	Head *NodeL
-	Tail *NodeL
-}
-
-func ListPushBack(l *List, data interface{}) {
-	node := &NodeL{Data: data, Next: nil}
-	if l.Tail == nil {
-		l.Head = node
-		l.Tail = node
-	} else {
-		l.Tail.Next = node
-		l.Tail = node
-	}
-}
+package piscine
 
 func ListAt(l *NodeL, pos int) *NodeL {
 	if l == nil {
@@ -39,17 +14,4 @@ func ListAt(l *NodeL, pos int) *NodeL {
 		noeud = noeud.Next
 	}
 	return noeud
-}
-
-func main() {
-	link := &List{}
-
-	ListPushBack(link, "hello")
-	ListPushBack(link, "how are")
-	ListPushBack(link, "you")
-	ListPushBack(link, 1)
-
-	fmt.Println(ListAt(link.Head, 3).Data)
-	fmt.Println(ListAt(link.Head, 1).Data)
-	fmt.Println(ListAt(link.Head, 7))
 }
