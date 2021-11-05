56b364139024f588e28cadc1667d82eeb9a29625
LCHARLOPEAU <luc.charlopeau@ynov.com>
2021-11-05 15:57:44 +0100

    printnbr

--- printnbr.go
+++ printnbr.go
@@ -13,13 +13,6 @@ func PrintNbr(n int) {
 			n /= 10
 		}
 		for i := 0; i < len(order); i++ {
-			for j := 0; j < len(order); j++ {
-				if order[i] < order[j] {
-					order[i], order[j] = order[j], order[i]
-				}
-			}
-		}
-		for i := 0; i < len(order); i++ {
 			z01.PrintRune(rune(48 + order[i]))
 		}
 	}
