a7db08f3be8e75ae5778df3d879dafa5dae5b82a
LCHARLOPEAU <luc.charlopeau@ynov.com>
2021-11-07 22:03:06 +0100

    a toi

--- atoi.go
+++ atoi.go
@@ -4,9 +4,13 @@ func Atoi(s string) int {
 	num := 0
 	minus := 0
 	plus := 0
+	nb := 0
 	for _, i := range s {
-		if (i < '0' || i > '9') && (i != '-' && i != '+') || (minus == 2 || plus == 2) || (minus == 1 && plus == 1) {
+		if (i < '0' || i > '9') && (i != '-' && i != '+') || (minus == 2 || plus == 2) || (minus == 1 && plus == 1) || (nb == 1 && i == '-' || i == '+') {
 			return 0
+		}
+		if i < '0' || i > '9' {
+			nb = 1
 		}
 		if i == '-' {
 			minus++
