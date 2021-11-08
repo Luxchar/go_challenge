569dac5160f7a612098807c32632a3c9072820c2
LCHARLOPEAU <luc.charlopeau@ynov.com>
2021-11-08 12:16:23 +0100

    trouve prime suivant

--- findnextprime.go
+++ findnextprime.go
@@ -3,11 +3,10 @@ package piscine
 func FindNextPrime(nb int) int {
 	if nb == 1 || nb == 0 {
 		return 1
-	} else {
-		for i := nb; i < nb/2; i++ {
-			if IsPrime(i) {
-				return i
-			}
+	}
+	for i := nb; i < nb/2; i++ {
+		if IsPrime(i) {
+			return i
 		}
 	}
 	return 1
