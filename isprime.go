5685c0a7fe9924135bf0fcc2146a532bf2f4337a
LCHARLOPEAU <luc.charlopeau@ynov.com>
2021-11-08 12:36:32 +0100

    trouve prime suivant

--- isprime.go
+++ isprime.go
@@ -1,10 +1,14 @@
 package piscine
 
 func IsPrime(nb int) bool {
+	grand := 0
 	if nb == 1 || nb <= 0 {
 		return false
 	} else {
-		for i := 2; i < nb; i++ {
+		if nb > 100 {
+			grand = nb / 2
+		}
+		for i := 2; i < grand; i++ {
 			if nb%i == 0 {
 				return false
 			}
