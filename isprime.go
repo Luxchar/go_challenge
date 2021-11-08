b1d42822ec4b1230bbdecd7791515b3bc53819c3
LCHARLOPEAU <luc.charlopeau@ynov.com>
2021-11-08 12:34:06 +0100

    trouve prime suivant

--- isprime.go
+++ isprime.go
@@ -4,7 +4,7 @@ func IsPrime(nb int) bool {
 	if nb == 1 || nb <= 0 {
 		return false
 	} else {
-		for i := 2; i < nb/2; i++ {
+		for i := 2; i < nb; i++ {
 			if nb%i == 0 {
 				return false
 			}
