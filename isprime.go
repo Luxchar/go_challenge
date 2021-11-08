27de668928d5f4226dd48194e537a7da75f0a5f6
LCHARLOPEAU <luc.charlopeau@ynov.com>
2021-11-08 14:18:48 +0100

    est premier

--- isprime.go
+++ isprime.go
@@ -1,14 +1,10 @@
 package piscine
 
 func IsPrime(nb int) bool {
-	a := 2
 	if nb == 1 || nb <= 0 {
 		return false
 	}
-	if nb > 150 {
-		a = nb - 100
-	}
-	for i := a; i < nb; i++ {
+	for i := 2; i < nb/2; i++ {
 		if nb%i == 0 {
 			return false
 		}
