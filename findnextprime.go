1787bfd784c5a7a76dc4eff66f2882b7aa95405d
LCHARLOPEAU <luc.charlopeau@ynov.com>
2021-11-08 12:10:35 +0100

    trouve prime suivant

--- findnextprime.go
+++ findnextprime.go
@@ -4,7 +4,7 @@ func FindNextPrime(nb int) int {
 	if nb == 1 || nb == 0 {
 		return 1
 	} else {
-		for i := 2; i < nb/2; i++ {
+		for i := nb; i < nb/2; i++ {
 			if IsPrime(i) {
 				return i
 			}
