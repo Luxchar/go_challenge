51ffb14b19a7ecb14fb5539ef719e5fa45f01c69
LCHARLOPEAU <luc.charlopeau@ynov.com>
2021-11-08 14:25:43 +0100

    est premier

--- isprime.go
+++ isprime.go
@@ -4,12 +4,8 @@ func IsPrime(nb int) bool {
 	if nb == 1 || nb <= 0 {
 		return false
 	}
-	if nb > 1000 {
-		for i := 2; i < nb/2; i++ {
-			if nb%i == 0 {
-				return false
-			}
-		}
+	if nb > 100 {
+		nb = nb / 2
 	}
 	for i := 2; i < nb; i++ {
 		if nb%i == 0 {
