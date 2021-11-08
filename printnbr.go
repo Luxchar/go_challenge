5b4aa77a7896ce966a077a15d5bf87a071071569
LCHARLOPEAU <luc.charlopeau@ynov.com>
2021-11-08 10:06:21 +0100

    ecris nombre

--- printnbr.go
+++ printnbr.go
@@ -4,6 +4,29 @@ import "github.com/01-edu/z01"
 
 func PrintNbr(n int) {
 	order := []int{}
+	if n == -9223372036854775808 {
+		z01.PrintRune(rune('-'))
+		z01.PrintRune(rune('9'))
+		z01.PrintRune(rune('2'))
+		z01.PrintRune(rune('2'))
+		z01.PrintRune(rune('3'))
+		z01.PrintRune(rune('3'))
+		z01.PrintRune(rune('7'))
+		z01.PrintRune(rune('2'))
+		z01.PrintRune(rune('0'))
+		z01.PrintRune(rune('3'))
+		z01.PrintRune(rune('6'))
+		z01.PrintRune(rune('8'))
+		z01.PrintRune(rune('5'))
+		z01.PrintRune(rune('4'))
+		z01.PrintRune(rune('7'))
+		z01.PrintRune(rune('7'))
+		z01.PrintRune(rune('5'))
+		z01.PrintRune(rune('8'))
+		z01.PrintRune(rune('0'))
+		z01.PrintRune(rune('8'))
+		return
+	}
 	if n < 0 {
 		z01.PrintRune(rune('-'))
 		n = n * -1
