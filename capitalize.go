dea6b0004298c8a47707b14bae29b8a84061f086
LCHARLOPEAU <luc.charlopeau@ynov.com>
2021-11-09 06:24:04 +0100

    capitalizateur

--- capitalize.go
+++ capitalize.go
@@ -3,6 +3,11 @@ package piscine
 func Capitalize(s string) string {
 	arr := []rune(s)
 	for i, v := range arr {
+		if v >= 'A' && v <= 'Z' {
+			arr[i] = v + 32
+		}
+	}
+	for i, v := range arr {
 		if v >= 'a' && v <= 'z' && i != 0 {
 			if (arr[i-1] < 'A' || arr[i-1] > 'Z') && (arr[i-1] < 'a' || arr[i-1] > 'z') && (arr[i-1] < '0' || arr[i-1] > '9') {
 				arr[i] = v - 32
