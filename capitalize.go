603d44964150a00ab85ab0ef08ea82c57e1717e4
LCHARLOPEAU <luc.charlopeau@ynov.com>
2021-11-09 06:17:26 +0100

    capitalizateur

---
+++ capitalize.go
@@ -0,0 +1,15 @@
+package piscine
+
+func Capitalize(s string) string {
+	arr := []rune(s)
+	for i, v := range arr {
+		if v >= 'a' && v <= 'z' && i != 0 {
+			if (arr[i-1] < 'A' || arr[i-1] > 'Z') && (arr[i-1] < 'a' || arr[i-1] > 'z') && (arr[i-1] < '0' || arr[i-1] > '9') {
+				arr[i] = v - 32
+			}
+		} else if i == 0 && v >= 'a' && v <= 'z' {
+			arr[i] = v - 32
+		}
+	}
+	return string(arr)
+}
