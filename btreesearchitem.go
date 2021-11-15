03047502d012279b2a4a7653f590d0e9d6eeac0c
LCHARLOPEAU <luc.charlopeau@ynov.com>
2021-11-15 15:51:05 +0100

    arbre

--- btreesearchitem.go
+++ btreesearchitem.go
@@ -1,11 +1,14 @@
 package piscine
 
 func BTreeSearchItem(root *TreeNode, elem string) *TreeNode {
-	if root.Data == elem || root == nil {
-		return &TreeNode{Data: elem}
+	if root.Data == elem {
+		return root
 	}
 	if root.Data > elem {
 		return BTreeSearchItem(root.Left, elem)
 	}
-	return BTreeSearchItem(root.Right, elem)
+	if root.Data < elem {
+		return BTreeSearchItem(root.Right, elem)
+	}
+	return nil
 }
