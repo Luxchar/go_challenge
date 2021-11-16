ec032f2bb10c641e06b39fed9df7b8a0491b49f4
LCHARLOPEAU <luc.charlopeau@ynov.com>
2021-11-16 11:31:02 +0100

    bug

--- btreesearchitem.go
+++ btreesearchitem.go
@@ -1,14 +1,11 @@
 package piscine
 
 func BTreeSearchItem(root *TreeNode, elem string) *TreeNode {
-	if root.Data == elem {
+	if root.Data == elem || root == nil {
 		return root
 	}
 	if root.Data > elem {
 		return BTreeSearchItem(root.Left, elem)
 	}
-	if root.Data < elem {
-		return BTreeSearchItem(root.Right, elem)
-	}
-	return nil
+	return BTreeSearchItem(root.Right, elem)
 }
