0e281acf6142b612cc632c83efaa92c9f55ef15e
LCHARLOPEAU <luc.charlopeau@ynov.com>
2021-12-02 15:35:16 +0100

    test

--- btreesearchitem.go
+++ btreesearchitem.go
@@ -1,10 +1,5 @@
 package piscine
 
-type TreeNode struct {
-	Left, Right, Parent *TreeNode
-	Data                string
-}
-
 func BTreeSearchItem(root *TreeNode, elem string) *TreeNode {
 	if root.Data == elem || root == nil {
 		return root
